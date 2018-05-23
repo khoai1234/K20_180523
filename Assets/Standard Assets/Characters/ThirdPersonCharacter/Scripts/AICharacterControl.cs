2dde9e231bfd94723cd66ba02f8d377e3f9850d0
duinguyen98 <duinguyen98@gmail.com>
2018-05-23 16:23:33 +0700

    first commit

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform target;                                    // target to aim for
+        public Transform player;
+
+        NavMeshAgent nav;
+
+
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+            player = GameObject.FindWithTag("Player").transform;
+            nav = GetComponent<NavMeshAgent>();
+
+        }
+
+
+        private void Update()
+        {
+            if (target != null)
+                agent.SetDestination(target.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+            else
+                character.Move(Vector3.zero, false, false);
+
+            //nav.SetDestination (player.position);
+        }
+
+
+        public void SetTarget(Transform target)
+        {
+            this.target = player;
+        }
+    }
+}
